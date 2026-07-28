0446048498fffb4c7dab4213f64e4bca6f54ac7d
vijayarajugoud <vijayarajubolem2000@gmail.com>
2026-07-28 10:43:48 +0000

    js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+alert("Welcome to nri it learning management system");
+let heading = document.getElementById("Welcome");
+heading.innerHTML ="Welcome Future Software Enginners"
+console.log("heading element:", heading)
---
+++ js/text.js
@@ -0,0 +1,32 @@
+console.log("Hello world");
+let student_name = "Vijayaraju";
+console.log("Student name is: " + student_name);
+
+let student_age = 20;
+let course_name = "Python Full Stack";
+console.log("Student age: " + student_age + ", Course name: " + course_name);
+
+let fee = 10000;
+let discount = 10;
+let discounted_fee = fee - (fee * discount / 100);
+console.log("Discounted fee is: " + discounted_fee);
+
+let age = 18;
+if (age >= 18) {
+    console.log("Eligible for admission");
+} else {
+    console.log("Not eligible for admission");
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log("Iteration number: " + i);
+}
+
+const pi = 3.14;
+console.log("value of pi is: " + pi);
+
+let student = {
+    name: "Vijayaraju",
+    age: 20,
+    course: "Python Full Stack",
+};
