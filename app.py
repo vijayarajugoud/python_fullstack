fbae0b715b252c96c98518db7ed4391f669197cd
vijayarajugoud <vijayarajubolem2000@gmail.com>
2026-08-04 10:48:53 +0000

    update

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello, World!"
-if __name__ == '__main__':
+    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+@app.route("/register")
+def register():
+    if request.method=="POST":
+        name=request.form["name"]
+        email=request.form["email"]
+        password=request.form["password"]
+        dob=request.form["dob"]
+        gender=request.form["gender"]
+        course=request.form["course"]
+    return render_template("register.html")
+@app.route("/trainers")
+def trainers():
+    return render_template("trainers.html")
+if __name__=='__main__':
     app.run(debug=True)
